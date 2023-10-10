7eca2df0680afea9b5eeba1af8b6999c1b5d8e9d
nam30042003 <dothanhnam0678@gmail.com>
2023-10-10 12:45:18 +0700

    first commit

---
+++ QUANLYNHANVIENN/Form1.Designer.cs
@@ -0,0 +1,328 @@
+﻿namespace QUANLYNHANVIENN
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtMaNV = new System.Windows.Forms.TextBox();
+            this.txtHoTen = new System.Windows.Forms.TextBox();
+            this.cbbPhong = new System.Windows.Forms.ComboBox();
+            this.hsluong = new System.Windows.Forms.TextBox();
+            this.btnTimKiem = new System.Windows.Forms.Button();
+            this.btnThemNV = new System.Windows.Forms.Button();
+            this.gridDSNV = new System.Windows.Forms.DataGridView();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.dtpNS = new System.Windows.Forms.DateTimePicker();
+            this.btnXoa = new System.Windows.Forms.Button();
+            this.btnCapNhat = new System.Windows.Forms.Button();
+            this.btnKhoiTao = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            this.cbbChucVu = new System.Windows.Forms.ComboBox();
+            this.btnThoat = new System.Windows.Forms.Button();
+            this.cbbGioiTinh = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.gridDSNV)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(87, 15);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(75, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Mã Nhân Viên";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(87, 46);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(43, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Họ Tên";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(87, 146);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(60, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Phòng Ban";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(87, 208);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(70, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Hệ Số Lương";
+            // 
+            // txtMaNV
+            // 
+            this.txtMaNV.Location = new System.Drawing.Point(183, 15);
+            this.txtMaNV.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtMaNV.Name = "txtMaNV";
+            this.txtMaNV.Size = new System.Drawing.Size(151, 20);
+            this.txtMaNV.TabIndex = 4;
+            this.txtMaNV.TextChanged += new System.EventHandler(this.txtMaNV_TextChanged);
+            // 
+            // txtHoTen
+            // 
+            this.txtHoTen.Location = new System.Drawing.Point(183, 42);
+            this.txtHoTen.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtHoTen.Name = "txtHoTen";
+            this.txtHoTen.Size = new System.Drawing.Size(151, 20);
+            this.txtHoTen.TabIndex = 5;
+            // 
+            // cbbPhong
+            // 
+            this.cbbPhong.FormattingEnabled = true;
+            this.cbbPhong.Location = new System.Drawing.Point(183, 146);
+            this.cbbPhong.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.cbbPhong.Name = "cbbPhong";
+            this.cbbPhong.Size = new System.Drawing.Size(151, 21);
+            this.cbbPhong.TabIndex = 6;
+            // 
+            // hsluong
+            // 
+            this.hsluong.Location = new System.Drawing.Point(183, 204);
+            this.hsluong.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.hsluong.Name = "hsluong";
+            this.hsluong.Size = new System.Drawing.Size(151, 20);
+            this.hsluong.TabIndex = 7;
+            // 
+            // btnTimKiem
+            // 
+            this.btnTimKiem.Location = new System.Drawing.Point(440, 37);
+            this.btnTimKiem.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnTimKiem.Name = "btnTimKiem";
+            this.btnTimKiem.Size = new System.Drawing.Size(70, 32);
+            this.btnTimKiem.TabIndex = 8;
+            this.btnTimKiem.Text = "Tìm Kiếm";
+            this.btnTimKiem.UseVisualStyleBackColor = true;
+            this.btnTimKiem.Click += new System.EventHandler(this.btnTimKiem_Click);
+            // 
+            // btnThemNV
+            // 
+            this.btnThemNV.Location = new System.Drawing.Point(433, 73);
+            this.btnThemNV.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnThemNV.Name = "btnThemNV";
+            this.btnThemNV.Size = new System.Drawing.Size(99, 29);
+            this.btnThemNV.TabIndex = 9;
+            this.btnThemNV.Text = "Thêm Nhân Viên";
+            this.btnThemNV.UseVisualStyleBackColor = true;
+            this.btnThemNV.Click += new System.EventHandler(this.btnThemNV_Click);
+            // 
+            // gridDSNV
+            // 
+            this.gridDSNV.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.gridDSNV.Location = new System.Drawing.Point(89, 244);
+            this.gridDSNV.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.gridDSNV.Name = "gridDSNV";
+            this.gridDSNV.RowTemplate.Height = 24;
+            this.gridDSNV.Size = new System.Drawing.Size(548, 183);
+            this.gridDSNV.TabIndex = 10;
+            this.gridDSNV.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.gridDSNV_CellContentClick);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(87, 76);
+            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(51, 13);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Giới Tính";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(87, 110);
+            this.label6.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(56, 13);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "Năm Sinh ";
+            // 
+            // dtpNS
+            // 
+            this.dtpNS.CustomFormat = "dd/MM/yyyy";
+            this.dtpNS.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dtpNS.Location = new System.Drawing.Point(183, 106);
+            this.dtpNS.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.dtpNS.Name = "dtpNS";
+            this.dtpNS.Size = new System.Drawing.Size(151, 20);
+            this.dtpNS.TabIndex = 13;
+            // 
+            // btnXoa
+            // 
+            this.btnXoa.Location = new System.Drawing.Point(433, 107);
+            this.btnXoa.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnXoa.Name = "btnXoa";
+            this.btnXoa.Size = new System.Drawing.Size(92, 29);
+            this.btnXoa.TabIndex = 16;
+            this.btnXoa.Text = "Xóa Nhân Viên";
+            this.btnXoa.UseVisualStyleBackColor = true;
+            this.btnXoa.Click += new System.EventHandler(this.btnXoa_Click);
+            // 
+            // btnCapNhat
+            // 
+            this.btnCapNhat.Location = new System.Drawing.Point(433, 141);
+            this.btnCapNhat.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnCapNhat.Name = "btnCapNhat";
+            this.btnCapNhat.Size = new System.Drawing.Size(114, 29);
+            this.btnCapNhat.TabIndex = 17;
+            this.btnCapNhat.Text = "Cập Nhật Nhân Viên";
+            this.btnCapNhat.UseVisualStyleBackColor = true;
+            this.btnCapNhat.Click += new System.EventHandler(this.btnCapNhat_Click);
+            // 
+            // btnKhoiTao
+            // 
+            this.btnKhoiTao.Location = new System.Drawing.Point(433, 176);
+            this.btnKhoiTao.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnKhoiTao.Name = "btnKhoiTao";
+            this.btnKhoiTao.Size = new System.Drawing.Size(78, 27);
+            this.btnKhoiTao.TabIndex = 18;
+            this.btnKhoiTao.Text = "Khởi Tạo";
+            this.btnKhoiTao.UseVisualStyleBackColor = true;
+            this.btnKhoiTao.Click += new System.EventHandler(this.btnKhoiTao_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(92, 176);
+            this.label7.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(47, 13);
+            this.label7.TabIndex = 20;
+            this.label7.Text = "Chức vụ";
+            // 
+            // cbbChucVu
+            // 
+            this.cbbChucVu.FormattingEnabled = true;
+            this.cbbChucVu.Location = new System.Drawing.Point(183, 176);
+            this.cbbChucVu.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.cbbChucVu.Name = "cbbChucVu";
+            this.cbbChucVu.Size = new System.Drawing.Size(151, 21);
+            this.cbbChucVu.TabIndex = 21;
+            // 
+            // btnThoat
+            // 
+            this.btnThoat.Location = new System.Drawing.Point(433, 208);
+            this.btnThoat.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnThoat.Name = "btnThoat";
+            this.btnThoat.Size = new System.Drawing.Size(56, 31);
+            this.btnThoat.TabIndex = 22;
+            this.btnThoat.Text = "Thoát";
+            this.btnThoat.UseVisualStyleBackColor = true;
+            this.btnThoat.Click += new System.EventHandler(this.btnThoat_Click);
+            // 
+            // cbbGioiTinh
+            // 
+            this.cbbGioiTinh.FormattingEnabled = true;
+            this.cbbGioiTinh.Items.AddRange(new object[] {
+            "Nam",
+            "Nữ"});
+            this.cbbGioiTinh.Location = new System.Drawing.Point(183, 70);
+            this.cbbGioiTinh.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.cbbGioiTinh.Name = "cbbGioiTinh";
+            this.cbbGioiTinh.Size = new System.Drawing.Size(151, 21);
+            this.cbbGioiTinh.TabIndex = 23;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(791, 444);
+            this.Controls.Add(this.cbbGioiTinh);
+            this.Controls.Add(this.btnThoat);
+            this.Controls.Add(this.cbbChucVu);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.btnKhoiTao);
+            this.Controls.Add(this.btnCapNhat);
+            this.Controls.Add(this.btnXoa);
+            this.Controls.Add(this.dtpNS);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.gridDSNV);
+            this.Controls.Add(this.btnThemNV);
+            this.Controls.Add(this.btnTimKiem);
+            this.Controls.Add(this.hsluong);
+            this.Controls.Add(this.cbbPhong);
+            this.Controls.Add(this.txtHoTen);
+            this.Controls.Add(this.txtMaNV);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.gridDSNV)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtMaNV;
+        private System.Windows.Forms.TextBox txtHoTen;
+        private System.Windows.Forms.ComboBox cbbPhong;
+        private System.Windows.Forms.TextBox hsluong;
+        private System.Windows.Forms.Button btnTimKiem;
+        private System.Windows.Forms.Button btnThemNV;
+        private System.Windows.Forms.DataGridView gridDSNV;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.DateTimePicker dtpNS;
+        private System.Windows.Forms.Button btnXoa;
+        private System.Windows.Forms.Button btnCapNhat;
+        private System.Windows.Forms.Button btnKhoiTao;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.ComboBox cbbChucVu;
+        private System.Windows.Forms.Button btnThoat;
+        private System.Windows.Forms.ComboBox cbbGioiTinh;
+    }
+}
+
---
+++ QUANLYNHANVIENN/Form1.cs
@@ -0,0 +1,307 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace QUANLYNHANVIENN
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection connect;
+        string connectionString = "Data Source=DESKTOP-N39PB8F\\THANHDEATH;Initial Catalog=QLNV;Integrated Security=True";
+        DataSet ds = new DataSet();
+        SqlDataAdapter da = new SqlDataAdapter();
+        SqlCommand command;
+        DataTable table = new DataTable();
+        public Form1()
+        {
+            InitializeComponent();
+           connect = new SqlConnection(connectionString);
+            connect.Open();
+            load(); 
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+            {
+              
+             
+               
+                string sqlDSNV = "select * from DSNV";
+                string sqlDMP = "select * from DMPHONG";
+                string sqlCV = "select * from CHUCVU";
+                da = new SqlDataAdapter(sqlDSNV, connectionString);
+                da.Fill(ds, "DSNV");
+                da = new SqlDataAdapter(sqlDMP, connectionString);
+                da.Fill(ds, "DMPHONG");
+                da = new SqlDataAdapter(sqlCV, connectionString);
+                da.Fill(ds, "CHUCVU");
+                gridDSNV.DataSource = ds.Tables["DSNV"];
+                cbbPhong.DataSource = ds.Tables["DMPHONG"];
+                cbbPhong.DisplayMember = "TenPhong";
+                cbbPhong.ValueMember = "MaPhong";
+
+               cbbChucVu.DataSource = ds.Tables["CHUCVU"];
+               cbbChucVu.DisplayMember = "TenChucVu";
+               cbbChucVu.ValueMember = "MaChucVu";
+
+           }
+        void load()
+        {
+            command = connect.CreateCommand();
+            command.CommandText = "select * from DSNV";
+            da.SelectCommand = command;
+            table.Clear();
+            da.Fill(table);
+            gridDSNV.DataSource = table;
+        }
+
+
+
+        private void btnThemNV_Click(object sender, EventArgs e)
+        {
+            string connectionString = "Data Source=DESKTOP-N39PB8F\\THANHDEATH;Initial Catalog=QLNV;Integrated Security=True";
+            SqlConnection connection = new SqlConnection(connectionString);
+            
+            try
+            {
+                connection.Open();
+                string sqlinsert = "INSERT INTO DSNV(MaNV, HoTen, MaPhong, HeSoLuong, MaChucVu, NgaySinh, GioiTinh)" +
+                    " VALUES(@MaNV, @HoTen, @MaPhong, @HeSoLuong, @MaChucVU, @NgaySinh, @GioiTinh)";
+                SqlCommand command = new SqlCommand(sqlinsert, connection);
+               //// command = connection.CreateCommand();
+                command.Parameters.AddWithValue("@MaNV", txtMaNV.Text);
+                command.Parameters.AddWithValue("@HoTen", txtHoTen.Text);
+                command.Parameters.AddWithValue("@MaPhong", cbbPhong.SelectedValue.ToString());
+                command.Parameters.AddWithValue("@HeSoLuong", hsluong.Text);
+                command.Parameters.AddWithValue("@MaChucVu", cbbChucVu.SelectedValue.ToString());
+                command.Parameters.AddWithValue("@NgaySinh", dtpNS.Value);
+                command.Parameters.AddWithValue("GioiTinh", cbbGioiTinh.Text);
+                int rowsAffected = command.ExecuteNonQuery();
+                if (rowsAffected > 0)
+                {
+                    MessageBox.Show("Thêm mới nhân viên thành công!");
+                    ds.Clear();
+                    
+                    load();
+                   
+                }
+                else
+                {
+                    MessageBox.Show("Thêm nhân viên thất bại");
+                }
+            }
+            finally { connection.Close(); }
+        }
+
+       
+
+        private void btnXoa_Click(object sender, EventArgs e)
+        {
+            string connectionString = "Data Source=DESKTOP-N39PB8F\\THANHDEATH;Initial Catalog=QLNV;Integrated Security=True"; ;
+            SqlConnection connection = new SqlConnection(connectionString);
+
+            try
+            {
+               
+                connection.Open();
+
+                
+                string query = "DELETE FROM DSNV WHERE MaNV = @MaNV";
+
+                
+                SqlCommand command = new SqlCommand(query, connection);
+                command.Parameters.AddWithValue("@MaNV", txtMaNV.Text);
+
+                
+                int result = command.ExecuteNonQuery();
+
+                
+                if (result > 0)
+                {
+                    MessageBox.Show("Xóa nhân viên thành công!");
+                    ds.Clear();
+                    load();
+                }
+                else
+                {
+                    MessageBox.Show("Không tìm thấy nhân viên có mã " + txtMaNV.Text);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Lỗi: " + ex.Message);
+            }
+            finally
+            {
+                
+                connection.Close();
+            }
+        }
+
+
+        
+        //SqlConnection connection = new SqlConnection(connectionString);
+        //try
+        //{
+        //    connection.Open();
+        //    string query = "delete from DSNV where MaNV = @MaNV";
+        //    SqlCommand command = new SqlCommand(query, connection);
+        //    command.Parameters.AddWithValue("@MaNV", txtMaNV.Text);
+        //    SqlDataReader reader = command.ExecuteReader();
+        //    if (reader.Read())
+        //    {
+        //        string sqlxoa = "delete * from DSNV where MaNV = N'" + reader["MaNV"].ToString() + "'";
+        //        da = new SqlDataAdapter(sqlxoa, connectionString);
+        //        da.Fill(ds, "DSNV1");
+        //        gridDSNV.DataSource = ds.Tables["DSNV1"];
+
+        //    }
+        //}
+        //catch (Exception ex)
+        //{
+        //    MessageBox.Show(ex.Message);
+
+        //}
+    
+
+    private void btnCapNhat_Click(object sender, EventArgs e)
+        {
+            string connectionString = "Data Source=DESKTOP-N39PB8F\\THANHDEATH;Initial Catalog=QLNV;Integrated Security=True";
+            SqlConnection connection = new SqlConnection(connectionString);
+            try
+            {
+                connection.Open();
+                string sqlupdate = "UPDATE DSNV SET HoTen = @HoTen, MaPhong = @MaPhong, HeSoLuong = @HeSoLuong, MaChucVu = @MaChucVu, NgaySinh = @NgaySinh, GioiTinh = @GioiTinh WHERE MaNV = @MaNV";
+                SqlCommand command = new SqlCommand(sqlupdate, connection);
+               // command = connection.CreateCommand();
+                command.Parameters.AddWithValue("@MaNV", txtMaNV.Text);
+                command.Parameters.AddWithValue("@HoTen", txtHoTen.Text);
+                command.Parameters.AddWithValue("@MaPhong", cbbPhong.SelectedValue.ToString());
+                command.Parameters.AddWithValue("@HeSoLuong", hsluong.Text);
+                command.Parameters.AddWithValue("@MaChucVu", cbbChucVu.SelectedValue.ToString());
+                command.Parameters.AddWithValue("@NgaySinh", dtpNS.Value);
+                command.Parameters.AddWithValue("GioiTinh", cbbGioiTinh.Text);
+                int rowsAffected = command.ExecuteNonQuery();
+                if (rowsAffected > 0)
+                {
+                    MessageBox.Show("Cập nhật nhân viên thành công!");
+                    txtMaNV.Clear();
+                    txtHoTen.Clear();
+                    hsluong.Clear();
+                    gridDSNV.DataSource = ds.Tables["DSNV"];
+                }
+                else
+                {
+                    MessageBox.Show("Cập nhật nhân viên thất bại");
+                }
+            }
+            finally { connection.Close(); }
+        }
+        private void btnKhoiTao_Click(object sender, EventArgs e)
+            {
+                txtMaNV.Text = "";
+                txtHoTen.Text = "";
+                dtpNS.Text = "01/01/1900";
+                cbbPhong.Text = "";
+                hsluong.Text = "";
+                cbbGioiTinh.Text = "Nam";
+                cbbChucVu.Text = "";
+            }
+
+        private void btnThoat_Click(object sender, EventArgs e)
+            {
+                this.Close();
+            }
+
+        //private void cbbPhong_SelectedIndexChanged(object sender, EventArgs e)
+        //    {
+        //        string sqlDSNV = "select * from DSNV where MaPhong = N'" + cbbPhong.SelectedValue.ToString() + "'";
+        //        da = new SqlDataAdapter(sqlDSNV, connectionString);
+        //        da.Fill(ds, "DSNV1");
+        //        gridDSNV.DataSource = ds.Tables["DSNV1"];
+        //    }
+
+        private void btnTimKiem_Click(object sender, EventArgs e)
+        {
+            string connectionString = "Server=DESKTOP-N39PB8F\\THANHDEATH; Database=QLNSS; Trusted_Connection =true";
+                SqlConnection connection = new SqlConnection(connectionString);
+               try
+               {
+
+                  connection.Open();
+                   string query = "select * from DSNV where Hoten = @Hoten";
+                  SqlCommand command = new SqlCommand(query, connection);
+                  command.Parameters.AddWithValue("@Hoten", txtHoTen.Text);
+                    SqlDataReader reader = command.ExecuteReader();
+                    if (reader.Read())
+                    {
+                      string sqltimkiem = "select * from DSNV where Hoten = N'" + reader["Hoten"].ToString() + "'";
+                        da = new SqlDataAdapter(sqltimkiem, connectionString);
+                        da.Fill(ds, "DSNV1");
+                       gridDSNV.DataSource = ds.Tables["DSNV1"];
+
+                    }
+                    else
+                   {
+                       MessageBox.Show("Không tìm thấy tên nhân viên bạn đang cần", "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                  }
+                }
+               catch (Exception ex)
+               {
+                    MessageBox.Show(ex.Message);
+               }
+                finally { connection.Close();
+
+                }
+        }
+
+        private void gridDSNV_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            txtMaNV.ReadOnly = true;
+            int i;
+            i = gridDSNV.CurrentRow.Index;
+            txtMaNV.Text = gridDSNV.Rows[i].Cells[0].Value.ToString();
+            txtHoTen.Text = gridDSNV.Rows[i].Cells[1].Value.ToString();
+            dtpNS.Text = gridDSNV.Rows[i].Cells[5].Value.ToString();
+            cbbPhong.Text = gridDSNV.Rows[i].Cells[2].Value.ToString();
+            hsluong.Text = gridDSNV.Rows[i].Cells[3].Value.ToString();
+            cbbGioiTinh.Text = gridDSNV.Rows[i].Cells[6].Value.ToString();
+            cbbChucVu.Text = gridDSNV.Rows[i].Cells[4].Value.ToString();
+        }
+
+        private void txtMaNV_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
